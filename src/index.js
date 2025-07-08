0fac9430d7b3b316724f6f006072fcc0751fa0c7
Poojesh K S <143901761+1pooj234@users.noreply.github.com>
2025-07-08 19:46:16 +0530

    Update index.js
    cors options update

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // if you send cookies or auth headers
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(adminRouter);
 app.use(employeeRouter);
