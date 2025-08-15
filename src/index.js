c0c47272349e23dedeb98f657ffb222f49b074e4
Poojesh K S <143901761+1pooj234@users.noreply.github.com>
2025-08-15 15:35:30 +0530

    Update index.js url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://user-management-app-poojeshs-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // if you send cookies or auth headers
